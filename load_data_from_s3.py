23ffcf6bee39616a6a960a54a453775cc6fdf064
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-10-31 14:56:37 -0600

    Update load_data_from_s3.py

--- load_data_from_s3.py
+++ load_data_from_s3.py
@@ -8,6 +8,7 @@ default_args = {
     'depends_on_past': False,
     'start_date': airflow.utils.dates.days_ago(1),
     'retry_delay': timedelta(minutes=5)
+    'fernet_key': 'Y0ZCeE0wdFJZVFphTkVveE5IaFJjRGhrWmpCamJ6RlJhak5vZVZwclIxaz0='
 }
 
 dag = DAG('dag_insert_data_postgres', 
