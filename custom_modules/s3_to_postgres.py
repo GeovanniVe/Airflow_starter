ecfa52f93ec6a59414ef68e91dab1068dd0fefc7
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-02 23:51:48 -0600

    Update s3_to_postgres.py

--- custom_modules/s3_to_postgres.py
+++ custom_modules/s3_to_postgres.py
@@ -213,7 +213,7 @@ class S3ToPostgresOperator(BaseOperator):
                                   dtype=schema)
 #         df_products.replace(r'^\s*$', np.nan, regex=True, inplace=True)
 #         df_products['CustomerID'] = df_products['CustomerID'].fillna(np.nan)
-        df_products['CustomerID'].replace([None], inplace=True)
+        df_products['CustomerID'].fillna([None], inplace=True)
         df_products.dropna(inplace=True)
         return df_products, list_content
 
