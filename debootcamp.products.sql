6de409ea5f6001aad19d2419eb0254e2c9b7d688
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-02 22:55:12 -0600

    Update debootcamp.products.sql

--- debootcamp.products.sql
+++ debootcamp.products.sql
@@ -9,9 +9,9 @@ CREATE TABLE IF NOT EXISTS debootcamp.products
     InvoiceNo VARCHAR(10),
     StockCode VARCHAR(20),
     Description VARCHAR(1000),
-    Quantity INT,
+    Quantity BIGINT,
     InvoiceDate TIMESTAMP,
     UnitPrice NUMERIC(8, 3),
-    CustomerID INT,
+    CustomerID BIGINT,
     Country VARCHAR(255)
 );
