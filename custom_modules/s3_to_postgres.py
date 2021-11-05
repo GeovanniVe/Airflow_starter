c0e2646e9713a353880d9eea47004d0779730e99
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-04 23:38:03 -0600

    Update s3_to_postgres.py

--- custom_modules/s3_to_postgres.py
+++ custom_modules/s3_to_postgres.py
@@ -136,7 +136,7 @@ class S3ToPostgresOperator(BaseOperator):
             None
         """
         s3_key_bucket = self.pg_s3_input()
-        #df_products, list_content = self.s3_object_to_df(s3_key_bucket)
+        df_products, list_content = self.s3_object_to_df(s3_key_bucket)
 #         self.create_db_table(df_products)
 #         self.print_table()
 
