35043ce9844c235bbc4f31c2dacd7262ab1de515
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-09 23:42:53 -0600

    Update emr_on_eks_test.py

--- emr_on_eks_test.py
+++ emr_on_eks_test.py
@@ -8,6 +8,8 @@ from airflow import DAG
 from airflow.providers.amazon.aws.operators.emr_containers import EMRContainerOperator
 from airflow.utils.dates import days_ago
 
+os.environ["AWS_DEFAULT_REGION"] = "us-east-2"
+
 # [START howto_operator_emr_eks_env_variables]
 VIRTUAL_CLUSTER_ID = os.getenv("VIRTUAL_CLUSTER_ID", "virtual_cluster_test")
 JOB_ROLE_ARN = os.getenv("JOB_ROLE_ARN", "arn:aws:iam::012345678912:role/emr_eks_default_role")
