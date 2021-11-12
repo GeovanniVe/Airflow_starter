ee7c7992370f7ae4597bb91cbda033de0b73183e
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-12 12:56:45 -0600

    Update spark_ex.py

--- spark_ex.py
+++ spark_ex.py
@@ -6,16 +6,6 @@ from pyspark.sql import sqlContext
 
 
 if __name__ == "__main__":
-    # Every record of this DataFrame contains the label and
-    # features represented by a vector.
-    df = sqlContext.createDataFrame(data, ["label", "features"])
-
-    # Set parameters for the algorithm.
-    # Here, we limit the number of iterations to 10.
-    lr = LogisticRegression(maxIter=10)
-
-    # Fit the model to the data.
-    model = lr.fit(df)
-
-    # Given a dataset, predict each point's label, and show the results.
-    model.transform(df).show()
+    # Test the spark
+    df = spark.createDataFrame([{"hello": "world"} for x in range(1000)])
+    df.show(3, False)
