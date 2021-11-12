6fb35eaf89181ccdba1ab7e7204173de1b4a92ad
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-12 12:59:46 -0600

    Update spark_ex.py

--- spark_ex.py
+++ spark_ex.py
@@ -2,10 +2,11 @@ import sys
 from random import random
 from operator import add
 
-from pyspark.sql import sqlContext
+from pyspark.sql import SparkSession
 
 
 if __name__ == "__main__":
     # Test the spark
+    spark = SparkSession.builder.appName("Test_spark").master("local[*]").getOrCreate()
     df = spark.createDataFrame([{"hello": "world"} for x in range(1000)])
     df.show(3, False)
