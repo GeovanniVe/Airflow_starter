a754937ed1b717aa0847262e8801d11ce7837421
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-12 14:11:10 -0600

    Update spark_ex.py

--- spark_ex.py
+++ spark_ex.py
@@ -11,15 +11,11 @@ if __name__ == "__main__":
 
     spark = SparkSession\
         .builder\
-        .appName("PythonWordCount")\
+        .appName("PythonHelloWorld")\
         .getOrCreate()
 
-    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
-    counts = lines.flatMap(lambda x: x.split(' ')) \
-                  .map(lambda x: (x, 1)) \
-                  .reduceByKey(add)
-    output = counts.collect()
-    for (word, count) in output:
-        print("%s: %i" % (word, count))
+    # Test the spark
+    df = spark.createDataFrame([{"hello": "world"} for x in range(10)])
+    df.show(3, False)
 
     spark.stop()
