a4687571e9b467d3a605ef4c52bf14d8a6db1e27
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-14 00:01:25 -0600

    Update emr_on_eks_test.py

--- emr_on_eks_test.py
+++ emr_on_eks_test.py
@@ -12,7 +12,7 @@ os.environ["AWS_DEFAULT_REGION"] = "us-east-2"
 
 # [START howto_operator_emr_eks_env_variables]
 VIRTUAL_CLUSTER_ID = "alhxiu8omjb5133efm8cfxjp8"  # os.getenv("VIRTUAL_CLUSTER_ID", "virtual_cluster_test")
-JOB_ROLE_ARN = os.getenv("JOB_ROLE_ARN", "arn:aws:iam::855157247171:role/EMRContainers-JobExecutionRole")
+JOB_ROLE_ARN = "arn:aws:iam::855157247171:role/EMRContainers-JobExecutionRole"
 # [END howto_operator_emr_eks_env_variables]
 
 
