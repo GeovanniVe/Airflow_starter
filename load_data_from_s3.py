f9c4c3ee049bb32e9af32a65073194d347f18f7a
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-19 13:42:28 -0600

    Update load_data_from_s3.py

--- load_data_from_s3.py
+++ load_data_from_s3.py
@@ -26,14 +26,14 @@ with dag:
                                     prefix='s',
                                     aws_conn_id='aws_default')
     
-    process_data = S3ToPostgresOperator(task_id='dag_s3_to_postgres',
-                                        schema='debootcamp',
-                                        table='products',
-                                        s3_bucket='de-bootcamp-airflow-data',
-                                        s3_key='sample.csv',
-                                        postgres_conn_id='postgres_default',
-                                        aws_conn_id='aws_default',
-                                        dag=dag)
+#     process_data = S3ToPostgresOperator(task_id='dag_s3_to_postgres',
+#                                         schema='debootcamp',
+#                                         table='products',
+#                                         s3_bucket='de-bootcamp-airflow-data',
+#                                         s3_key='sample.csv',
+#                                         postgres_conn_id='postgres_default',
+#                                         aws_conn_id='aws_default',
+#                                         dag=dag)
 
     
     
