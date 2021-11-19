86fc955aa63d7eaac809003c56a950e135b2053f
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-19 13:41:47 -0600

    Update load_data_from_s3.py

--- load_data_from_s3.py
+++ load_data_from_s3.py
@@ -17,7 +17,8 @@ default_args = {
 dag = DAG('dag_insert_data_postgres', 
           default_args=default_args,
           schedule_interval='@once',
-          catchup=False,)
+          catchup=False,
+          tags=['s3_postgres'])
 
 with dag:
     names = S3ListOperator(task_id='list_3s_files',
