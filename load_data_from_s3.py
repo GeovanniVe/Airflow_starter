12be2523d04910553decfcc148b42e22dc5cc4ea
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-19 13:43:48 -0600

    Update load_data_from_s3.py

--- load_data_from_s3.py
+++ load_data_from_s3.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from airflow import DAG
 import airflow.utils.dates
-from custom_modules.s3_to_postgres import S3ToPostgresOperator
+# from custom_modules.s3_to_postgres import S3ToPostgresOperator
 from airflow.contrib.operators.s3_list_operator import S3ListOperator
 from airflow.providers.amazon.aws.transfers.google_api_to_s3 import GoogleApiToS3Operator
 import os
