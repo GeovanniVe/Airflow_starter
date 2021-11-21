e0228d0d38548bb8015b67ee05ee0298455a4919
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-20 20:36:59 -0600

    Update emr_on_eks_test.py

--- emr_on_eks_test.py
+++ emr_on_eks_test.py
@@ -12,7 +12,7 @@ from airflow.utils.dates import days_ago
 os.environ["AWS_DEFAULT_REGION"] = "us-east-2"
 
 # [START howto_operator_emr_eks_env_variables]
-VIRTUAL_CLUSTER_ID = "twckapogtyv41lnsiwwsw23bw"  # os.getenv("VIRTUAL_CLUSTER_ID", "virtual_cluster_test")
+VIRTUAL_CLUSTER_ID = "b5rb051oodt2ql3vz116frk51"  # os.getenv("VIRTUAL_CLUSTER_ID", "virtual_cluster_test")
 JOB_ROLE_ARN = "arn:aws:iam::855157247171:role/EMRContainers-JobExecutionRole"
 # [END howto_operator_emr_eks_env_variables]
 
