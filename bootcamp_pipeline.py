d6de240847456254f5b939d0b98877b13d53a5e2
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-21 21:49:20 -0600

    Update bootcamp_pipeline.py

--- bootcamp_pipeline.py
+++ bootcamp_pipeline.py
@@ -60,8 +60,8 @@ def get_bucket_name():
     import logging
     from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
     s3 = AwsBaseHook(aws_conn_id="aws_default", client_type="s3")
-#     s3 = s3.conn()
-    response = s3.list_buckets()
+    s3_c = s3.conn()
+    response = s3_c.list_buckets()
     logging.info("{0}".format(response))
 #     s3 = boto3.resource('s3')
 #     for bucket in s3.buckets.all():
