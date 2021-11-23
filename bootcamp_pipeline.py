9e13a150dbc46453ae7905cf0a49fb4e372334a5
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-22 23:49:45 -0600

    Update bootcamp_pipeline.py

--- bootcamp_pipeline.py
+++ bootcamp_pipeline.py
@@ -5,14 +5,19 @@ import airflow.utils.dates
 # Append custom modules to path. If this is not done logs will not show up
 # possibly due to a timeout
 import sys
+
 sys.path.append("/opt/airflow/dags/repo/custom_modules")
 
 from airflow.providers.amazon.aws.operators.emr_containers import \
     EMRContainerOperator
 from s3_to_postgres import S3ToPostgresOperator
 from postgres_to_s3 import PostgresToS3Operator
-from airflow.contrib.operators.s3_list_operator import S3ListOperator
 from airflow.operators.python import PythonOperator
+
+# Pipeline for the data engineering bootcamp which transforms the
+# movie_reviews.csv file. At the same time as a parallel job it
+# transfers the user_purchase.csv file from S3 to postgres and back
+# to S3.
 
 
 # [START EMRContainer operator variables]
@@ -45,24 +50,22 @@ CONFIGURATION_OVERRIDES_ARG = {
         }
     ],
     "monitoringConfiguration": {
-        "cloudWatchMonitoringConfiguration": {
-            "logGroupName": "/emr-on-eks/eksworkshop-eksctl",
-            "logStreamNamePrefix": "pi"
-        },
         "s3MonitoringConfiguration": {
             "logUri": "s3://spark-test-samp"
         }
     }
 }
+
+
 # [END EMRContainerOperator config]
 
 def get_bucket_name():
     """
     Reads the bucket names in S3.
-    
-    Gets the raw and staging layer buckets' full name and assigns them 
+
+    Gets the raw and staging layer buckets' full name and assigns them
         to a dictionary.
-     
+
     Returns:
         buckets: dict
           Dictionary with the name of the raw and staging layer buckets.
@@ -71,16 +74,16 @@ def get_bucket_name():
     s3 = AwsBaseHook(aws_conn_id="aws_default", client_type="s3")
     response = s3.conn.list_buckets()
     bucket_names = [bucket["Name"] for bucket in response["Buckets"]]
-    raw_name = ""
-    staging_name = ""
+    buckets = {}
     for i in bucket_names:
         if i.startswith("raw-layer"):
-            raw_name = i
-        if i.startswith("staging-layer"):
-            staging_name = i
-    buckets = {"raw": raw_name, "staging": staging_name} 
+            buckets["raw"] = i
+        elif i.startswith("staging-layer"):
+            buckets["staging"] = i
     return buckets
 
+# DAG Start
+# ----------------------------------------------------------------------------
 default_args = {
     'owner': 'geovanni.velazquez',
     'depends_on_past': False,
@@ -98,7 +101,7 @@ with dag:
     get_bucket_names = PythonOperator(task_id="get_s3_bucket_names",
                                       python_callable=get_bucket_name,
                                       dag=dag)
-    
+
     # Save raw data to postgres from the user_purchase.csv file. Afterwards
     # the same data is extracted from postgres and sent to the staging layer
     # bucket created with terraform.
@@ -122,7 +125,7 @@ with dag:
                                          )
 
     # Classifies the movie_reviews.csv file by looking for the word "good".
-    # Assigns a 1 if the word is found else a 0. Saves file with cid and 
+    # Assigns a 1 if the word is found else a 0. Saves file with cid and
     # class (called the "positivity" column) columns only.
     reviews_job = EMRContainerOperator(
         task_id="movie_reviews_classification",
@@ -133,8 +136,8 @@ with dag:
         job_driver=JOB_DRIVER_ARG,
         name="movie_reviews.py"
     )
-    
+
     # fan out after getting the names of the buckets created with terraform
     get_bucket_names >> [process_data, reviews_job]
 
-    process_data >> pg_to_staging 
+    process_data >> pg_to_staging
