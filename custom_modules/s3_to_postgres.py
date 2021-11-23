21ace36e7e71c9f4dd0eadaff227d85ec77baa54
GeovanniVe <92698021+GeovanniVe@users.noreply.github.com>
2021-11-22 23:50:49 -0600

    Update s3_to_postgres.py

--- custom_modules/s3_to_postgres.py
+++ custom_modules/s3_to_postgres.py
@@ -5,9 +5,8 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.exceptions import AirflowException
 from airflow.models import BaseOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from airflow.providers.amazon.aws.utils.redshift import build_credentials_block
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.contrib.operators.s3_list_operator import S3ListOperator
+
 import pandas as pd
 import io
 import os.path
@@ -64,22 +63,22 @@ class S3ToPostgresOperator(BaseOperator):
     ui_color = '#99e699'
 
     def __init__(
-            self,
-            *,
-            schema: str,
-            table: str,
-            s3_bucket = None,
-            s3_key: str,
-            postgres_conn_id: str = 'postgres_default',
-            aws_conn_id: str = 'aws_default',
-            verify=None,
-            wildcard_match=False,
-            column_list: Optional[List[str]] = None,
-            copy_options: Optional[List] = None,
-            autocommit: bool = False,
-            method: str = 'APPEND',
-            upsert_keys: Optional[List[str]] = None,
-            **kwargs,
+        self,
+        *,
+        schema: str,
+        table: str,
+        s3_bucket=None,
+        s3_key: str,
+        postgres_conn_id: str = 'postgres_default',
+        aws_conn_id: str = 'aws_default',
+        verify=None,
+        wildcard_match=False,
+        column_list: Optional[List[str]] = None,
+        copy_options: Optional[List] = None,
+        autocommit: bool = False,
+        method: str = 'APPEND',
+        upsert_keys: Optional[List[str]] = None,
+        **kwargs,
     ) -> None:
 
         if 'truncate_table' in kwargs:
@@ -131,12 +130,14 @@ class S3ToPostgresOperator(BaseOperator):
         Returns:
             None
         """
+        # get raw layer unique bucket name in XCom
         task_instance = context['task_instance']
         value = task_instance.xcom_pull(task_ids="get_s3_bucket_names")
         self.s3_bucket = value["raw"]
+        
         s3_key_bucket = self.pg_s3_input(context)
         df_products, list_content = self.s3_object_to_df(s3_key_bucket)
-#         self.create_db_table(df_products)
+        self.create_db_table(df_products)
         self.print_table()
 
     def pg_s3_input(self, context):
@@ -159,7 +160,7 @@ class S3ToPostgresOperator(BaseOperator):
         self.s3 = S3Hook(aws_conn_id=self.aws_conn_id, verify=self.verify)
 
         self.log.info("Downloading S3 file: {0}".format(self.s3))
-        
+
         s3_key_bucket = None
         if self.wildcard_match:
             if self.s3.check_for_wildcard_key(self.s3_key, self.s3_bucket):
@@ -171,7 +172,7 @@ class S3ToPostgresOperator(BaseOperator):
                 raise AirflowException("The key {0} does not exist".
                                        format(self.s3_key))
             s3_key_bucket = self.s3.get_key(self.s3_key,
-                                                    self.s3_bucket)
+                                            self.s3_bucket)
 
         return s3_key_bucket
 
@@ -192,7 +193,7 @@ class S3ToPostgresOperator(BaseOperator):
                 S3 file as a single string.
         """
         self.log.info("s3_key_bucket: {0}".format(s3_key_bucket))
-        
+
         list_content = s3_key_bucket.get()['Body'].read() \
             .decode(encoding='utf-8', errors='ignore')
 
@@ -206,13 +207,13 @@ class S3ToPostgresOperator(BaseOperator):
             'CustomerID': 'float64',
             'Country': str
         }
-        
+
         df_products = pd.read_csv(io.StringIO(list_content),
                                   header=0,
                                   delimiter=',',
                                   low_memory=False,
                                   dtype=schema)
-        
+
         self.log.info("df: {0}".format(df_products))
         df_products.replace(np.nan, None, inplace=True)
         return df_products, list_content
@@ -229,9 +230,8 @@ class S3ToPostgresOperator(BaseOperator):
         """
         file_path = 'dags/repo/debootcamp.products.sql'
 
-        
         self.log.info("all content: {0}".format(os.listdir()))
-            
+
         with open(file_path, "r", encoding="UTF-8") as sql_file:
             sql_create_table_cmd = sql_file.read()
             sql_file.close()
@@ -245,15 +245,17 @@ class S3ToPostgresOperator(BaseOperator):
 
         self.current_table = self.schema + '.' + self.table
         df_row_list = [tuple(x) for x in df_products.to_numpy()]
-        
+
         # check table exist
         try:
             self.pg_hook.insert_rows(self.current_table, df_row_list,
-                                     target_fields=target_fields, commit_every=1000,
+                                     target_fields=target_fields,
+                                     commit_every=1000,
                                      replace=True)
         except:
             self.pg_hook.insert_rows(self.current_table, df_row_list,
-                                     target_fields=target_fields, commit_every=1000,
+                                     target_fields=target_fields,
+                                     commit_every=1000,
                                      replace=False)
 
     def print_table(self):
@@ -266,7 +268,7 @@ class S3ToPostgresOperator(BaseOperator):
         Returns:
             None
         """
-        request = "SELECT * FROM " + "debootcamp.products" +\
+        request = "SELECT * FROM " + "debootcamp.products" + \
                   " WHERE InvoiceNo = '536367' LIMIT 3"
         connection = self.pg_hook.get_conn()
         cursor = connection.cursor()
@@ -285,4 +287,3 @@ class S3ToPostgresOperator(BaseOperator):
                           Country: {7} ".
                           format(row[0], row[1], row[2], row[3],
                                  row[4], row[5], row[6], row[7]))
-            
